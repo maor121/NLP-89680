83417be24986e233d677d5d9a7f797ff3d664b83
maor121 <maors1152@gmail.com>
2017-11-19 14:02:21 +0200

    Prepare tag counters - WIP

--- Assignment1/code/MLETrain.py
+++ Assignment1/code/MLETrain.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 
 def read_input_file(input_filename):
     """Return a list of pairs, (word, tag)"""
@@ -16,15 +17,27 @@ def read_input_file(input_filename):
     except Exception as e:
         raise
 
-def count_tags(train_data):
-    from collections import Counter
-    tags = train_data[1]
-    return len(Counter(tags).keys())
-
 def list_to_ids(L):
     from collections import Counter
     L2I = {t: i for i, t in enumerate(Counter(L).keys())}
     return L2I
+
+def count_triplets(tags, T2I):
+    from collections import Counter
+    count_y1_y2_y3 = Counter()
+    for t1, t2, t3 in triplets(tags):
+        i1, i2, i3 = T2I[t1], T2I[t2], T2I[t3]
+        count_y1_y2_y3.update((i1,i2,i3)) #tuple, order does not matter for hashing
+    return count_y1_y2_y3
+
+def triplets(iterable):
+    "s -> (s0,s1,s2), (s1,s2,s3), (s2, s3,s4), ..."
+    from itertools import tee, izip
+    a, b, c = tee(iterable, 3)
+    next(b, None)
+    next(c, None)
+    next(c, None)
+    return izip(a, b, c)
 
 if __name__ == '__main__':
     args = sys.argv[1:]
@@ -38,8 +51,8 @@ if __name__ == '__main__':
     e_mle_filename = args[2]
 
     train_data = read_input_file(input_filename)
-    tags_count = count_tags(train_data)
 
     W2I = list_to_ids(train_data[0])
     T2I = list_to_ids(train_data[1])
 
+    count_triplets = count_triplets(train_data[1], T2I)
