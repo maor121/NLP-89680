19d4b7ffc4808fd3fbb450b5cc1f9d39874aabbd
maor121 <maors1152@gmail.com>
2017-11-19 13:09:07 +0200

    Prepare ids

--- Assignment1/code/MLETrain.py
+++ Assignment1/code/MLETrain.py
@@ -2,16 +2,29 @@ import sys
 
 def read_input_file(input_filename):
     """Return a list of pairs, (word, tag)"""
-    train_data = []
+    words = []
+    tags = []
     try:
         with open(input_filename, 'rb') as f:
             for line in f:
                 wordsAndTags = line.split()
-                train_data += [tuple(w_t.split("\\")) for w_t in wordsAndTags]
-        return train_data
+                for w_t in wordsAndTags:
+                    for word, tag in zip(*w_t.split("\\")):
+                        words.append(word)
+                        tags.append(tag)
+        return [words, tags]
     except Exception as e:
         raise
 
+def count_tags(train_data):
+    from collections import Counter
+    tags = train_data[1]
+    return len(Counter(tags).keys())
+
+def list_to_ids(L):
+    from collections import Counter
+    L2I = {t: i for i, t in enumerate(Counter(L).keys())}
+    return L2I
 
 if __name__ == '__main__':
     args = sys.argv[1:]
@@ -25,4 +38,8 @@ if __name__ == '__main__':
     e_mle_filename = args[2]
 
     train_data = read_input_file(input_filename)
+    tags_count = count_tags(train_data)
+
+    W2I = list_to_ids(train_data[0])
+    T2I = list_to_ids(train_data[1])
 
