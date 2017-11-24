9b300cf0586506bbc6bf1174eb18746da7fb2744
ofir121 <ofir2793@gmail.com>
2017-11-24 14:23:17 +0200

    HMMTagger viterbi WIP

--- Assignment1/code/HMMTag.py
+++ Assignment1/code/HMMTag.py
@@ -1,11 +1,22 @@
 from MLETrain import MLETrain
 import sys
 import utils
+import numpy as np
+
 
 class HMMTag:
     __mletrain = None
     def __init__(self, mletrain):
         self.__mletrain = mletrain
+    def getPrediction(self, sentence_words):
+        tags = self.__mletrain.getTags()
+        V = np.ndarray([len(sentence_words), len(tags), len(tags)])
+
+        for i in range(len(sentence_words)):
+            for t_prev in tags:
+                for t in tags:
+                    V_prev_slice = V[i-1,:,:]
+                    V[i, t_prev, t] = np.max()
 
 if __name__ == '__main__':
     args = sys.argv[1:]
@@ -22,3 +33,24 @@ if __name__ == '__main__':
 
     model = MLETrain(q_mle_filename, e_mle_filename)
     tagger = HMMTag(model)
+
+    sentences = utils.read_input_file(input_filename, is_tagged=True)
+
+    miss_total = 0
+    total = 0
+    sentences_processed = 0
+    sentences_count = len(sentences)
+    progress = None
+    for (words, tags) in sentences:
+        prediction = tagger.getPrediction(words)
+        # Numbers are more easily compared then strings
+        prediction_ids = model.getTagsIds(prediction)
+        tags_ids = model.getTagsIds(tags[2:])  # Skip START
+        miss_total += sum(1 for i, j in zip(prediction_ids, tags_ids) if i != j)
+        total += len(prediction_ids)
+        sentences_processed += 1
+
+        progress = utils.progress_hook(sentences_processed, sentences_count, progress)
+    hit_total = total - miss_total
+    accuracy = hit_total * 1.0 / total
+    print("accuracy: {} in {} words".format(str(accuracy), str(total)))
