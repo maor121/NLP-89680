a93987bb22dbcfcf5718684eb46a6a19c05d2ea9
ofir121 <ofir2793@gmail.com>
2017-11-25 17:29:37 +0200

    Use cache for Q

--- Assignment1/code/MLETrain.py
+++ Assignment1/code/MLETrain.py
@@ -7,33 +7,52 @@ import numpy as np
 
 VOCAB_SIZE = 12000
 
+
 class MLETrain:
     __q_counts = None
     __e_counts = None
     __T2I = None
     __W2I = None
     __words_trained_count = None
+
+    __Q_cache = None
+    __cache_hit_count = None
+    __cache_miss_count = None
+
     def __init__(self, q_mle_file, e_mle_file):
         self.__T2I, self.__W2I, self.__q_counts, self.__e_counts, self.__words_trained_count = \
             utils.read_mle_files(q_mle_file, e_mle_file)
+        tags_count = len(self.__T2I)
+        self.__Q_cache = np.full([tags_count, tags_count, tags_count], -1, dtype=np.float64)
+        self.__cache_hit_count = self.__cache_miss_count = 0
 
     def getP(self, sentence_words, predictions):
         predictions = [utils.START_TAG, utils.START_TAG].extend(predictions)
-        #[y0,y1,y2,y3,y4] -> [(y0,y1,y2),(y1,y2,y3),(y2,y3,y4)]
+        # [y0,y1,y2,y3,y4] -> [(y0,y1,y2),(y1,y2,y3),(y2,y3,y4)]
         predictions_triplets = utils.triplets(predictions)
         logSum = 0.0
-        for word, (t_prev_prev, t_prev ,t) in sentence_words, predictions_triplets:
+        for word, (t_prev_prev, t_prev, t) in sentence_words, predictions_triplets:
             logSum += np.log(self.getQ(t, t_prev, t_prev_prev)) + \
                       np.log(self.getE(word, t))
         return logSum
 
     def getQ(self, c, b, a):
+        c_id, b_id, a_id = [self.__T2I[t]-1 for t in (c,b,a)]
+
+        if self.__Q_cache[c_id, b_id, a_id] == -1:
+            self.__Q_cache[c_id, b_id, a_id] = self.__calc_Q(c, b, a)
+            self.__cache_miss_count += 1
+        else:
+            self.__cache_hit_count += 1
+        return self.__Q_cache[c_id, b_id, a_id]
+
+    def __calc_Q(self, c, b, a):
         c_count = self.__get_tag_count([c]) * 1.0
-        ba_count = self.__get_tag_count([b,a])
+        ba_count = self.__get_tag_count([b, a])
         b_count = self.__get_tag_count([b])
 
         three = self.__get_tag_count([c, b, a]) * 1.0 / ba_count if ba_count != 0 else 0
-        two = self.__get_tag_count([c,b]) * 1.0 / b_count if b_count != 0 else 0
+        two = self.__get_tag_count([c, b]) * 1.0 / b_count if b_count != 0 else 0
         one = c_count / self.__words_trained_count
         return np.average([three, two, one])
 
@@ -85,11 +104,11 @@ class MLETrain:
         for sentence in train_data_ids:
             words_ids = sentence[0]
             tags_ids = sentence[1]
-            #Q
+            # Q
             count_tag_triplets.update(utils.count_triplets(tags_ids))
             count_tag_pairs.update(utils.count_pairs(tags_ids))
             count_tag_single.update(utils.count_single(tags_ids))
-            #E
+            # E
             count_word_tags.update(utils.count_word_tags(words_ids, tags_ids))
 
         log.debug("Writing to file {}".format(q_mle_filename))
@@ -100,11 +119,12 @@ class MLETrain:
 
         log.debug("Done")
 
+
 if __name__ == '__main__':
     args = sys.argv[1:]
     if len(args) != 3:
         print "Wrong number of arguments. Use:\n" + \
-                "python MLETrain.py input_filename q_mle_filename e_mle_filename"
+              "python MLETrain.py input_filename q_mle_filename e_mle_filename"
         exit()
 
     input_filename = args[0]
@@ -113,7 +133,7 @@ if __name__ == '__main__':
 
     MLETrain.createModelFilesFromInput(input_filename, q_mle_filename, e_mle_filename)
 
-    #Testing
+    # Testing
     """
     model = MLETrain(q_mle_filename, e_mle_filename)
     print(model.getQ("DT","JJR",":"))
