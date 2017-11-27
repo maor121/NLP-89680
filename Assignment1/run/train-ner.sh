631efe6c2d2bdcc7ed0d47381e7f232c57f0fe93
ofir121 <ofir2793@gmail.com>
2017-11-27 15:40:36 +0200

    train-ner.sh

--- Assignment1/run/train-ner.sh
+++ Assignment1/run/train-ner.sh
@@ -1,8 +1,9 @@
 #!/home/ofir/Devl/git/Deep-Learning-89687/venv/bin/activate
 
-src_folder="../src/"
-data_folder="../data-ner/"
-output_folder="../output-ner/"
+proj_folder="/home/ofir/Devl/git/NLP-89680/Assignment1"
+src_folder="src/"
+data_folder="data-ner/"
+output_folder="output-ner/"
 
 train_file=$data_folder"train"
 q_mle_file=$output_folder"q.mle"
@@ -12,4 +13,5 @@ pyFile=$src_folder"hmm1/MLETrain.py"
 
 echo python $pyFile $train_file $q_mle_file $e_mle_file
 
+cd $proj_folder
 python $pyFile $train_file $q_mle_file $e_mle_file
