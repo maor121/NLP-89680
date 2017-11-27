ce270c3cf5a5e2dc45471f1f0fe3782a2bad7608
ofir121 <ofir2793@gmail.com>
2017-11-27 19:06:01 +0200

    eval.sh final

--- Assignment1/run/eval.sh
+++ Assignment1/run/eval.sh
@@ -10,18 +10,18 @@ source $venv_folder
 cd $proj_folder
 
 echo "---------------------------------- Greedy Tag ----------------------------------------"
-python $ner_file $eval_file $predict_greedy_tag_file
+python $ner_file $eval_file $predict_greedy_tag_file $is_span
 echo ""
 
 echo "----------------------------------- HMM Tag ------------------------------------------"
-python $ner_file $eval_file $predict_hmm_tag_file
+python $ner_file $eval_file $predict_hmm_tag_file $is_span
 echo ""
 
 echo "---------------------------- Greedy Max Entropy Tag ----------------------------------"
-python $ner_file $eval_file $predict_greedy_max_ent_tag_file
+python $ner_file $eval_file $predict_greedy_max_ent_tag_file $is_span
 echo ""
 
 echo "----------------------------------- MEMM Tag -----------------------------------------"
-python $ner_file $eval_file $predict_memm_tag_file
+python $ner_file $eval_file $predict_memm_tag_file $is_span
 echo ""
 
