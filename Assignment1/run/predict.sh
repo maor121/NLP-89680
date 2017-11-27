db69aa3d1aed8b128deaeffc50d535f9e92ae111
ofir121 <ofir2793@gmail.com>
2017-11-27 17:56:06 +0200

    predict.sh wip

--- Assignment1/run/predict.sh
+++ Assignment1/run/predict.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source test.config
+source ner.config
 source general.config
 
 source $venv_folder
@@ -8,13 +8,13 @@ source $venv_folder
 cd $proj_folder
 
 echo "HMM Greedy"
-#python $greedy_tag_file $test_file $q_mle_file $e_mle_file $predict_greedy_tag_file
+python $greedy_tag_file $test_file $q_mle_file $e_mle_file $predict_greedy_tag_file
 
 echo "HMM Viterbi"
-#python $hmm_tag_file $test_file $q_mle_file $e_mle_file $predict_hmm_tag_file
+python $hmm_tag_file $test_file $q_mle_file $e_mle_file $predict_hmm_tag_file
 
 echo "MEMM Greedy"
-#python $greedy_max_ent_tag_file $test_file $sklearn_model_file $predict_greedy_max_ent_tag_file $features_map_file
+python $greedy_max_ent_tag_file $test_file $sklearn_model_file $predict_greedy_max_ent_tag_file $features_map_file
 
 echo "MEMM Viterbi"
 python $memm_tag_file $test_file $sklearn_model_file $predict_memm_tag_file $features_map_file
