7c92f9bbe5d4a296e6f0293a4da31055fe998e2b
ofir121 <ofir2793@gmail.com>
2017-11-27 17:36:10 +0200

    predict.sh wip

--- Assignment1/run/predict.sh
+++ Assignment1/run/predict.sh
@@ -7,5 +7,8 @@ source $venv_folder
 
 cd $proj_folder
 
-#python $greedy_tag_file $test_file $q_mle_file $e_mle_file $predict_greedy_tag_file
-python $hmm_tag_file $test_file $q_mle_file $e_mle_file $predict_greedy_tag_file
+echo "HMM Greedy"
+python $greedy_tag_file $test_file $q_mle_file $e_mle_file $predict_greedy_tag_file
+
+echo "HMM Viterbi"
+python $hmm_tag_file $test_file $q_mle_file $e_mle_file $predict_hmm_tag_file
