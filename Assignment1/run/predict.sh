c8aab355ba9febb4300e4fc3f561bd95da9f43d6
ofir121 <ofir2793@gmail.com>
2017-11-27 17:29:42 +0200

    predict.sh wip

---
+++ Assignment1/run/predict.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+source test.config
+source general.config
+
+source $venv_folder
+
+cd $proj_folder
+
+python $greedy_tag_file $test_file $q_mle_file $e_mle_file $predict_greedy_tag_file
