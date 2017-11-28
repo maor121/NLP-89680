bdce861979cd33764ccf21b8774e3b23fd7b894d
ofir121 <ofir2793@gmail.com>
2017-11-28 17:59:19 +0200

    sh some printing

--- Assignment1/run/train.sh
+++ Assignment1/run/train.sh
@@ -7,13 +7,19 @@ source $venv_folder
 cd $proj_folder
 
 #HMM
-echo "MLETrain.py"
+echo "MLETrain.py ${mle_train_file} ${train_file} ${q_mle_file} ${e_mle_file}"
 python $mle_train_file $train_file $q_mle_file $e_mle_file
+echo ""
 
 #MEMM
-echo "ExtractFeatures.py"
+echo "ExtractFeatures.py ${extract_features_file} ${train_file} ${features_file}"
 python $extract_features_file $train_file $features_file
-echo "ConvertFeatures.py"
+echo ""
+
+echo "ConvertFeatures.py ${features_file} ${features_vec_file} ${features_map_file}"
 python $convert_features_file $features_file $features_vec_file $features_map_file
-echo "Train_Solver.py"
+echo ""
+
+echo "Train_Solver.py ${features_vec_file} ${sklearn_model_file}"
 python $train_solver_file $features_vec_file $sklearn_model_file
+echo ""
