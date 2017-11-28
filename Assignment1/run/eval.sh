fb436b5d811a36165bb5145529e342ae5518db1d
ofir121 <ofir2793@gmail.com>
2017-11-28 17:49:22 +0200

    bugs in sh files

--- Assignment1/run/eval.sh
+++ Assignment1/run/eval.sh
@@ -9,19 +9,23 @@ cd $proj_folder
 for pair in $eval_files; do
     input_file=${pair#*+}   # remove prefix ending in "+"
     tagged_file=${pair%+*}   # remove suffix starting with "+"
+
+    input_filename=${input_file#*/} # remove prefix ending with /
+    pred_file_start="${output_folder}${input_filename}"
+
     echo "---------------------------------- Greedy Tag ----------------------------------------"
-    python $ner_file $tagged_file "${input_file}.greedy_tag.pred" $is_span
+    python $ner_file $tagged_file "${pred_file_start}.greedy_tag.pred" $is_span
     echo ""
 
     echo "----------------------------------- HMM Tag ------------------------------------------"
-    python $ner_file $tagged_file "${input_file}.hmm_tag.pred" $is_span
+    python $ner_file $tagged_file "${pred_file_start}.hmm_tag.pred" $is_span
     echo ""
 
     echo "---------------------------- Greedy Max Entropy Tag ----------------------------------"
-    python $ner_file $tagged_file "${input_file}.greedy_max_ent_tag.pred" $is_span
+    python $ner_file $tagged_file "${pred_file_start}.greedy_max_ent_tag.pred" $is_span
     echo ""
 
     echo "----------------------------------- MEMM Tag -----------------------------------------"
-    python $ner_file $tagged_file "${input_file}.memm_tag.pred" $is_span
+    python $ner_file $tagged_file "${pred_file_start}.memm_tag.pred" $is_span
     echo ""
 done
--- Assignment1/run/predict.sh
+++ Assignment1/run/predict.sh
@@ -9,17 +9,19 @@ cd $proj_folder
 for file in $test_files; do
     echo "------------------------------------------------------------------------------------------------------"
     file_name=${file#*/}   # remove prefix ending in "/"
+    pred_file_start="${output_folder}${file_name}"
+
     echo "File: ${file_name}"
     echo "HMM Greedy"
-    python $greedy_tag_file $file $q_mle_file $e_mle_file "${file}.greedy_tag.pred"
+    python $greedy_tag_file $file $q_mle_file $e_mle_file "${pred_file_start}.greedy_tag.pred"
 
     echo "HMM Viterbi"
-    python $hmm_tag_file $file $q_mle_file $e_mle_file "${file}.hmm_tag.pred"
+    python $hmm_tag_file $file $q_mle_file $e_mle_file "${pred_file_start}.hmm_tag.pred"
 
     echo "MEMM Greedy"
-    python $greedy_max_ent_tag_file $file $sklearn_model_file "${file}.greedy_max_ent_tag.pred" $features_map_file
+    python $greedy_max_ent_tag_file $file $sklearn_model_file "${pred_file_start}.greedy_max_ent_tag.pred" $features_map_file
 
     echo "MEMM Viterbi"
-    python $memm_tag_file $file $sklearn_model_file "${file}.memm_tag.pred" $features_map_file
+    python $memm_tag_file $file $sklearn_model_file "${pred_file_start}.memm_tag.pred" $features_map_file
     echo "------------------------------------------------------------------------------------------------------"
 done
