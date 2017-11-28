a25bb812d86f36d02e0c29926c64d0f595d7af8d
ofir121 <ofir2793@gmail.com>
2017-11-28 15:33:25 +0200

    eval.sh for blind file

--- Assignment1/run/eval.sh
+++ Assignment1/run/eval.sh
@@ -7,19 +7,22 @@ source $venv_folder
 
 cd $proj_folder
 
-echo "---------------------------------- Greedy Tag ----------------------------------------"
-python $ner_file $eval_file $predict_greedy_tag_file $is_span
-echo ""
+for pair in $eval_files; do
+    input_file=${pair#*+}   # remove prefix ending in "+"
+    tagged_file=${pair%+*}   # remove suffix starting with "+"
+    echo "---------------------------------- Greedy Tag ----------------------------------------"
+    python $ner_file $tagged_file "${input_file}.greedy_tag.pred" $is_span
+    echo ""
 
-echo "----------------------------------- HMM Tag ------------------------------------------"
-python $ner_file $eval_file $predict_hmm_tag_file $is_span
-echo ""
+    echo "----------------------------------- HMM Tag ------------------------------------------"
+    python $ner_file $tagged_file "${input_file}.hmm_tag.pred" $is_span
+    echo ""
 
-echo "---------------------------- Greedy Max Entropy Tag ----------------------------------"
-python $ner_file $eval_file $predict_greedy_max_ent_tag_file $is_span
-echo ""
+    echo "---------------------------- Greedy Max Entropy Tag ----------------------------------"
+    python $ner_file $tagged_file "${input_file}.greedy_max_ent_tag.pred" $is_span
+    echo ""
 
-echo "----------------------------------- MEMM Tag -----------------------------------------"
-python $ner_file $eval_file $predict_memm_tag_file $is_span
-echo ""
-
+    echo "----------------------------------- MEMM Tag -----------------------------------------"
+    python $ner_file $tagged_file "${input_file}.memm_tag.pred" $is_span
+    echo ""
+done
