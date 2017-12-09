396f3c4e6bb54e8d01faeceb298e420abd28cd56
maor121 <maors1152@gmail.com>
2017-12-09 17:41:15 +0200

    tagger1 parameters

--- Assignment2/code/tagger1.py
+++ Assignment2/code/tagger1.py
@@ -1,21 +1,39 @@
+import sys
 from torch.utils.data import TensorDataset
-
 from model_runner import ModelRunner
 from model import load_dataset
+import torch.utils.data
+
+def parse_arg_bool(val):
+    if val not in ("True", "False"):
+        print "bool arg is either True or False. got {}".format(val)
+        exit()
+    return val == "True"
+
+def parse_arg_eval_mode(val, allowed_values):
+    if val not in allowed_values:
+        print "eval_mode not in {}, got {}".format(allowed_values, val)
+        exit()
+    return val
 
 if __name__ == '__main__':
-    import torch.utils.data
+    sys.argv = sys.argv[1:]
+    if len(sys.argv)  < 6:
+        print "Wrong number of arguments, usage:\n" +\
+            "tagger1.py is_cuda(True\\False) train_file predict_file is_ner(True\\False) number_of_epoches eval_mode(blind\\everyepoch\\plot)"
+        exit()
+    else:
+        is_cuda = parse_arg_bool(sys.argv[0])
+        train_filename = sys.argv[1]
+        test_filename = sys.argv[2]
+        is_ner = parse_arg_bool(sys.argv[0])  # Used for eval
+        epoches = int(sys.argv[4])
+        eval_mode = parse_arg_eval_mode(sys.argv[5], ["blind", "everyepoch", "plot"])
 
-    train_filename = "../data/ner/train"
-    test_filename = "../data/ner/dev"
-    is_ner = True #Used for eval
-
-    is_cuda = True
     window_size = 2
     embedding_depth = 50
     learning_rate = 0.001
     batch_size = 500
-    epoches = 20
 
     W2I, T2I, train, train_labels = load_dataset(train_filename, window_size)
     __, __, test, test_labels = load_dataset(test_filename, window_size, W2I=W2I, T2I=T2I)
@@ -38,7 +56,7 @@ if __name__ == '__main__':
 
     runner = ModelRunner(window_size, learning_rate, is_cuda)
     runner.initialize_random(num_words, num_tags, embedding_depth)
-    runner.train_and_eval(trainloader, epoches, testloader, omit_tag_id, eval_mode="plot")
+    runner.train_and_eval(trainloader, epoches, testloader, omit_tag_id, eval_mode=eval_mode)
 
     print('Finished Training')
 
