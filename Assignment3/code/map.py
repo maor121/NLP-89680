8a7b5d29c292b3448379bb8c24d56e88f21cf4bb
ofir121 <ofir2793@gmail.com>
2018-01-04 17:05:50 +0200

    final touches

--- Assignment3/code/map.py
+++ Assignment3/code/map.py
@@ -6,7 +6,8 @@ def calc_precision_from_rel(rel_arr):
     for i, r in enumerate(rel_arr):
         if r == 1:
             total_correct += 1
-        sum += float(total_correct) / (i+1)
+            sum += float(total_correct) / (i+1)
+        # sum += precR * rel, so if r ==0, sum += 0
 
     assert len(rel_arr) == 20
     return sum / len(rel_arr)
@@ -22,14 +23,13 @@ if __name__ == '__main__':
     rel_piano_window = [int(s) for s in "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1".split()]
     rel_piano_tree = [int(s) for s in "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0".split()]
 
-    print("ap,car,sentence", calc_precision_from_rel(rel_car_sentence))
-    print("ap,car,window", calc_precision_from_rel(rel_car_window))
-    print("ap,car,tree", calc_precision_from_rel(rel_car_tree))
-    print("ap,piano,sentence", calc_precision_from_rel(rel_piano_sentence))
-    print("ap,piano,window", calc_precision_from_rel(rel_piano_window))
-    print("ap,piano,tree", calc_precision_from_rel(rel_piano_tree))
+    print("ap,car,sentence, %.3f" % calc_precision_from_rel(rel_car_sentence))
+    print("ap,car,window, %.3f" % calc_precision_from_rel(rel_car_window))
+    print("ap,car,tree, %.3f" % calc_precision_from_rel(rel_car_tree))
+    print("ap,piano,sentence, %.3f" % calc_precision_from_rel(rel_piano_sentence))
+    print("ap,piano,window, %.3f" % calc_precision_from_rel(rel_piano_window))
+    print("ap,piano,tree, %.3f" % calc_precision_from_rel(rel_piano_tree))
 
-
-    print("map,sentence", np.average([calc_precision_from_rel(rel) for rel in [rel_car_sentence, rel_piano_sentence]]))
-    print("map,window", np.average([calc_precision_from_rel(rel) for rel in [rel_car_window, rel_piano_window]]))
-    print("map,tree", np.average([calc_precision_from_rel(rel) for rel in [rel_car_tree, rel_piano_tree]]))
+    print("map,sentence, %.3f" % np.average([calc_precision_from_rel(rel) for rel in [rel_car_sentence, rel_piano_sentence]]))
+    print("map,window, %.3f" % np.average([calc_precision_from_rel(rel) for rel in [rel_car_window, rel_piano_window]]))
+    print("map,tree, %.3f" % np.average([calc_precision_from_rel(rel) for rel in [rel_car_tree, rel_piano_tree]]))
