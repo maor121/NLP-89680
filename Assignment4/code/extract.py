7f9f655a185552a2a77a65b69e30a968db150dfa
maor121 <maors1152@gmail.com>
2018-01-16 15:22:16 +0200

    extract.py wip

--- Assignment4/code/extract.py
+++ Assignment4/code/extract.py
@@ -46,20 +46,41 @@ def print_ner_entities(sentence, ner_entities):
 
 
 def process_sentence(sentence):
+    result = {}
     ner_entities = extract_ners_from_sentence(sentence)
     #print_ner_entities(sentence, ner_entities)
     # Some sentences have as many as 12 NERS in one sentence
     import itertools
     ner_pairs = list(itertools.combinations(ner_entities, 2))
-    for ner1, ner2 in ner_pairs:
-        name_1 = extract_ner_name(sentence, ner1)
-        name_2 = extract_ner_name(sentence, ner2)
-        entity_type_1 = extract_ner_type(sentence, ner1)
-        entity_type_2 = extract_ner_type(sentence, ner2)
-        type_concated = entity_type_1+entity_type_2
-        constituent_path = extract_constituent_path(sentence, ner1, ner2)
-        chunk_path = extract_base_sysntactic_chunk_path(sentence, ner1, ner2)
-        dep_path = extract_typed_dependency_path(sentence, ner1, ner2)
+    for tmpNer1, tmpNer2 in ner_pairs:
+        for ner1, ner2 in [(tmpNer1, tmpNer2), (tmpNer2, tmpNer1)]: #pair, reverse_pair
+            name_1 = extract_ner_name(sentence, ner1)
+            name_2 = extract_ner_name(sentence, ner2)
+            entity_type_1 = extract_ner_type(sentence, ner1)
+            entity_type_2 = extract_ner_type(sentence, ner2)
+            type_concated = entity_type_1+entity_type_2
+            constituent_path = extract_constituent_path(sentence, ner1, ner2)
+            chunk_path = extract_base_sysntactic_chunk_path(sentence, ner1, ner2)
+            dep_path = extract_typed_dependency_path(sentence, ner1, ner2)
+            result[(name_1, name_2)] = \
+                (entity_type_1, entity_type_2, type_concated, constituent_path, chunk_path, dep_path)
+
+    return result
+
+def read_annotations_file(filename):
+    relation_by_sent_id = {}
+    with open(filename) as anno_file:
+        for line in anno_file:
+            line = line.strip()
+            arr = line.split('\t', 5) # Last element in the whole sentence
+            sent_id = arr[0]
+            ner1 = arr[1]
+            rel = arr[2]
+            ner2 = arr[3]
+            if sent_id not in relation_by_sent_id:
+                relation_by_sent_id[sent_id] = {}
+            relation_by_sent_id[sent_id][(ner1,ner2)] = rel
+    return relation_by_sent_id
 
 
 if __name__ == '__main__':
@@ -71,6 +92,7 @@ if __name__ == '__main__':
     NER_P_IDX = 7
     NER_IDX = 8
 
+    features_by_sent_id = {}
     with open("../data/Corpus.TRAIN.processed") as inp_file:
         sentence = []
         saw_empty_line = True
@@ -89,6 +111,10 @@ if __name__ == '__main__':
                 sentence.append(word)
             else:
                 if not saw_empty_line:
-                    process_sentence(sentence)
+                    features = process_sentence(sentence)
+                    features_by_sent_id[sent_id] = features
                     sentence = []
                 saw_empty_line = True
+        print(features_by_sent_id)
+
+    print(read_annotations_file("../data/TRAIN.annotations"))
