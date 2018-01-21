2331f9bcb7f98140c66f92a5b20abb6281d5bdf7
samuelsd1 <sdavid1407@gmail.com>
2018-01-21 06:04:55 +0200

    implemented typed dependency path exctraction. TODO:  * change the way the features are represented  * paths should be vectors - find out how

--- Assignment4/code/extract.py
+++ Assignment4/code/extract.py
@@ -1,3 +1,5 @@
+MAX_CLEN = 0
+
 
 def extract_ners_from_sentence(sentence):
     ner_entities = []
@@ -9,7 +11,7 @@ def extract_ners_from_sentence(sentence):
             last_ner = [i]
         else:
             if word[5] == 'I':
-                assert len(last_ner) != 0 #Illegal NER annotation, I without B
+                assert len(last_ner) != 0  # Illegal NER annotation, I without B
                 last_ner.append(i)
             else:  # O
                 if len(last_ner) > 0:
@@ -29,21 +31,22 @@ def extract_ner_type(sentence, ner):
 
 
 def extract_constituent_path(sentence, ner1, ner2):
+    global MAX_CLEN
     loc1 = ner1[-1]
     loc2 = ner2[-1]
 
     path_ids_up = []
     word = sentence[loc1]
-    while word[3] != 0: # not HEAD
-        path_ids_up.append(word[0]) #ID
-        word = sentence[word[3]-1]
+    while word[3] != 0:  # not HEAD
+        path_ids_up.append(word[0])  # ID
+        word = sentence[word[3] - 1]
     path_ids_up.append(0)
 
     path_ids_down = []
     word = sentence[loc2]
     while word[3] != 0:  # not HEAD
         path_ids_down.append(word[0])  # ID
-        word = sentence[word[3]-1]
+        word = sentence[word[3] - 1]
     path_ids_down.append(0)
 
     # Find lowest common ancestor
@@ -55,50 +58,83 @@ def extract_constituent_path(sentence, ner1, ner2):
             break
 
     remove_after_index = path_ids_up.index(ancestor)
-    path_ids_up = path_ids_up[:remove_after_index+1]
+    path_ids_up = path_ids_up[:remove_after_index + 1]
     remove_after_index = path_ids_down.index(ancestor)
-    path_ids_down = path_ids_down[:remove_after_index+1]
+    path_ids_down = path_ids_down[:remove_after_index + 1]
 
-    path_up_str = " ".join([(sentence[id-1][2] if id!=0 else "S") + "_UP" for id in path_ids_up]) #POS UP
-    path_down_str = " ".join([(sentence[id-1][2] if id!=0 else "S") + "_DOWN" for id in reversed(path_ids_down)])
+    lens = len(path_ids_down) + len(path_ids_up)
+    if lens > MAX_CLEN:
+        MAX_CLEN = lens
+
+    path_up_str = " ".join([(sentence[id - 1][2] if id != 0 else "S") + "_UP" for id in path_ids_up])  # POS UP
+    path_down_str = " ".join([(sentence[id - 1][2] if id != 0 else "S") + "_DOWN" for id in reversed(path_ids_down)])
 
     return path_up_str + " " + path_down_str
 
 
-def extract_base_sysntactic_chunk_path(sentence, ner1, ner2):
-    return "TODO_chunk"
-
-
 def extract_typed_dependency_path(sentence, ner1, ner2):
-    return "TODO_dep"
+    loc1 = ner1[-1]
+    loc2 = ner2[-1]
+
+    path_ids_right = []
+    word = sentence[loc1]
+    while word[3] != 0:  # not HEAD
+        path_ids_right.append(word[0])  # ID
+        word = sentence[word[3] - 1]
+
+    path_idx_left = []
+    word = sentence[loc2]
+    while word[3] != 0:  # not HEAD
+        path_idx_left.append(word[0])  # ID
+        word = sentence[word[3] - 1]
+
+    # Find lowest common ancestor
+    ancestor = None
+    ids_down_set = set(path_idx_left)
+    for id in path_ids_right:
+        if id in ids_down_set:
+            ancestor = id
+            break
+
+    if ancestor is None:
+        return ''
+
+    remove_after_index = path_ids_right.index(ancestor)
+    path_ids_right = path_ids_right[:remove_after_index][::-1]
+    remove_after_index = path_idx_left.index(ancestor)
+    path_idx_left = path_idx_left[:remove_after_index][::-1]
+
+    path_left = ' '.join('%s L-%s' % (sentence[id - 1][1], sentence[id - 1][4]) for id in path_idx_left)
+    path_right = ' '.join('%s R-%s' % (sentence[id - 1][1], sentence[id - 1][4]) for id in path_ids_right)
+    common = sentence[ancestor - 1][1]
+    return path_left + common + path_right
 
 
 def print_ner_entities(sentence, ner_entities):
     for ner in ner_entities:
         name = extract_ner_name(sentence, ner)
         ner_type = extract_ner_type(sentence, ner)
-        print(name +" "+ner_type)
+        print(name + " " + ner_type)
 
 
 def process_sentence(sentence):
     result = {}
     ner_entities = extract_ners_from_sentence(sentence)
-    #print_ner_entities(sentence, ner_entities)
+    # print_ner_entities(sentence, ner_entities)
     # Some sentences have as many as 12 NERS in one sentence
     import itertools
     ner_pairs = list(itertools.combinations(ner_entities, 2))
     for tmpNer1, tmpNer2 in ner_pairs:
-        for ner1, ner2 in [(tmpNer1, tmpNer2), (tmpNer2, tmpNer1)]: #pair, reverse_pair
+        for ner1, ner2 in [(tmpNer1, tmpNer2), (tmpNer2, tmpNer1)]:  # pair, reverse_pair
             name_1 = extract_ner_name(sentence, ner1)
             name_2 = extract_ner_name(sentence, ner2)
             entity_type_1 = extract_ner_type(sentence, ner1)
             entity_type_2 = extract_ner_type(sentence, ner2)
-            type_concated = entity_type_1+entity_type_2
-            constituent_path = extract_constituent_path(sentence, ner1, ner2)
-            chunk_path = extract_base_sysntactic_chunk_path(sentence, ner1, ner2)
+            type_concated = entity_type_1 + entity_type_2
             dep_path = extract_typed_dependency_path(sentence, ner1, ner2)
-            result[(name_1, name_2)] = \
-                (entity_type_1, entity_type_2, type_concated, constituent_path, chunk_path, dep_path)
+            constituent_path = extract_constituent_path(sentence, ner1, ner2)
+
+            result[(name_1, name_2)] = (entity_type_1, entity_type_2, type_concated, constituent_path, dep_path)
 
     return result
 
@@ -128,7 +164,8 @@ def read_processed_file(filename):
                 arr = line.split()
                 ner_type = arr[NER_IDX] if len(arr) > 8 else None
                 word = (
-                int(arr[ID_IDX]), arr[WORD_IDX], arr[POS_IDX], int(arr[HEAD_IDX]), arr[TREE_IDX], arr[NER_P_IDX], ner_type)
+                    int(arr[ID_IDX]), arr[WORD_IDX], arr[POS_IDX], int(arr[HEAD_IDX]), arr[TREE_IDX], arr[NER_P_IDX],
+                    ner_type)
                 sentence.append(word)
             else:
                 if not saw_empty_line:
@@ -144,14 +181,14 @@ def read_annotations_file(filename):
     with open(filename) as anno_file:
         for line in anno_file:
             line = line.strip()
-            arr = line.split('\t', 5) # Last element in the whole sentence
+            arr = line.split('\t', 5)  # Last element in the whole sentence
             sent_id = arr[0]
             ner1 = arr[1]
             rel = arr[2]
             ner2 = arr[3]
             if sent_id not in relation_by_sent_id:
                 relation_by_sent_id[sent_id] = {}
-            relation_by_sent_id[sent_id][(ner1,ner2)] = rel
+            relation_by_sent_id[sent_id][(ner1, ner2)] = rel
     return relation_by_sent_id
 
 
@@ -205,7 +242,7 @@ def compute_feature_key_to_anno_key(anno_by_sent_id, features_by_sent_id):
                 feature_key_to_anno_key[sent_id] = {}
             if both_passed_shared_word:
                 if found_f_key in feature_key_to_anno_key[sent_id]:
-                    print("Warning! double annotation for sentence: "+sent_id+" skipping.\n")
+                    print("Warning! double annotation for sentence: " + sent_id + " skipping.\n")
                 else:
                     feature_key_to_anno_key[sent_id][found_f_key] = anno_key
                     added_anno_count += 1
@@ -227,18 +264,18 @@ def convert_features_to_numbers(features_by_sent_id, anno_by_sent_id, feature_ke
     sent_ids = features_by_sent_id.keys()
     features_dim_count = len(features_by_sent_id[sent_ids[0]].values()[0])
 
-    allowed_anno = set(["Work_For", "Live_In"])
+    allowed_anno = {"Work_For", "Live_In"}
 
     if Counters is None:
-        Counters = np.ndarray(shape=(features_dim_count+1), dtype=object)
-        for i in range(features_dim_count+1):
+        Counters = np.ndarray(shape=(features_dim_count + 1), dtype=object)
+        for i in range(features_dim_count + 1):
             Counters[i] = StringCounter()
         for sent_id in sent_ids:
             for f_key, features in features_by_sent_id[sent_id].items():
                 [Counters[i].get_id_and_update(f) for i, f in enumerate(features)]
         # Filter rare features
         for i in range(features_dim_count):
-            Counters[i].filter_rare_words(2) #Min appearance: 2
+            Counters[i].filter_rare_words(2)  # Min appearance: 2
             Counters[i] = StringCounter(Counters[i].S2I.keys(), "*UNK*")
 
     X = []
@@ -247,7 +284,7 @@ def convert_features_to_numbers(features_by_sent_id, anno_by_sent_id, feature_ke
     removed_anno_count = 0
     for sent_id in sent_ids:
         for f_key, features in features_by_sent_id[sent_id].items():
-            features_as_ids = tuple([Counters[i].get_id(f) for i,f in enumerate(features)])
+            features_as_ids = tuple([Counters[i].get_id(f) for i, f in enumerate(features)])
             X.append(features_as_ids)
             anno_key = feature_key_to_anno_key[sent_id].get(f_key, None)
             anno = anno_by_sent_id[sent_id].get(anno_key, None)
@@ -263,22 +300,26 @@ def convert_features_to_numbers(features_by_sent_id, anno_by_sent_id, feature_ke
 
     return Counters, X, Y
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     features_by_sent_id = read_processed_file("../data/Corpus.TRAIN.processed")
     anno_by_sent_id = read_annotations_file("../data/TRAIN.annotations")
     feature_key_to_anno_key = compute_feature_key_to_anno_key(anno_by_sent_id, features_by_sent_id)
-    Counters, TrainX, TrainY = convert_features_to_numbers(features_by_sent_id, anno_by_sent_id, feature_key_to_anno_key)
+    Counters, TrainX, TrainY = convert_features_to_numbers(features_by_sent_id, anno_by_sent_id,
+                                                           feature_key_to_anno_key)
 
     features_by_sent_id = read_processed_file("../data/Corpus.DEV.processed")
     anno_by_sent_id = read_annotations_file("../data/DEV.annotations")
     feature_key_to_anno_key = compute_feature_key_to_anno_key(anno_by_sent_id, features_by_sent_id)
-    Counters, DevX, DevY = convert_features_to_numbers(features_by_sent_id, anno_by_sent_id, feature_key_to_anno_key,Counters=Counters)
+    Counters, DevX, DevY = convert_features_to_numbers(features_by_sent_id, anno_by_sent_id, feature_key_to_anno_key,
+                                                       Counters=Counters)
 
     import utils
+
     class_dict = utils.inverse_dict(Counters[-1].S2I)
 
     import svm
+
     svm.run_svm_print_result(TrainX, TrainY, DevX, DevY, class_dict)
 
     print("Done")
